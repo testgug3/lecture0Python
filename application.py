d8de52ebb196c0cb01ae1a1f1d4f2677000e86bc
Rahul <testgug3@gmail.com>
2018-12-12 08:24:54 +0545

    flask form templating

--- application.py
+++ application.py
@@ -1,12 +1,36 @@
-from flask import Flask, render_template
+import datetime
+
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    now = datetime.datetime.now()
+    new_year = now.month == 1 and now.day ==1
+    return render_template("index1.html", new_year=new_year)
 
-@app.route("/temp")
-def temp():
-    headline = "Hello Flask Templating!!"
-    return render_template("index.html", headline=headline)
+@app.route("/names")
+def names():
+    names = ["Alice","Bob","Charlie"]
+    return render_template("loop.html", names=names)
+
+@app.route("/more")
+def more():
+    return render_template("more.html")
+
+
+@app.route("/myForm")
+def myForm():
+    return render_template("myForm.html")
+
+
+@app.route("/myFormResult", methods=["GET","POST"])
+def myFormResult():
+    #if request method is get do this
+    if request.method == "GET":
+        return "Please Submit the form Insted."
+    #else do this
+    else:
+        name = request.form.get("name")
+        return render_template("myFormResult.html", name = name)
---
+++ application2.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/temp")
+def temp():
+    headline = "Hello Flask Templating!!"
+    return render_template("index.html", headline=headline)
+
+@app.route("/bye")
+def bye():
+    headline = "Good Bye!!"
+    return render_template("index.html", headline=headline)
+
